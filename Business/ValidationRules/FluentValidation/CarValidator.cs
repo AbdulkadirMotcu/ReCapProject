945f54c8433da631055865fa5207efea152d7da7
Abdulkadir Motcu <motcukadir7@gmail.com>
2022-07-16 05:50:02 +0300

    16 Temmuz 12.Gün ödev

--- Business/ValidationRules/FluentValidation/CarValidator.cs
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace Business.ValidationRules.FluentValidation
 {
-    public class CarValidator 
+    //Ekranlara göre DTO lara görede yapılabilir
+    public class CarValidator : AbstractValidator<Car>
     {
+        //Kurallar ctor içine yazılır...
+        public CarValidator()
+        {
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(0); //0'dan Büyük olmalı
+            RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(100).When(c => c.BrandId == 1);//100'den büyük olmalı ne zaman Brand
+            
+        }
     }
 }
